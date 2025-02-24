e6427315cffffbe3fa1acc1524f4abafc34feeef
Patrick Cuba <cubap@slu.edu>
2025-02-24 10:35:13 -0600

    passing around script and styles

--- components/gui/site/Page.js
+++ components/gui/site/Page.js
@@ -16,8 +16,14 @@ class TpenPageTemplate extends HTMLElement {
             }
         `
         this.prepend(style)
+        let css = this.dataset.css
+        if (css && !css.match(/\.css$/)) {
+            console.error('Invalid CSS file URL fragment:', css)
+            css = null
+        }
 
         shadow.innerHTML = `
+            ${css ? `<link rel="stylesheet" href="${this.dataset.css}">` : '' }
             <tpen-header title="${this.title ?? document.title}"></tpen-header> 
             <div class="page-content" style="padding: 1em; margin: 0 auto; min-height: 40vh;">
                 <slot></slot>
@@ -27,12 +33,9 @@ class TpenPageTemplate extends HTMLElement {
     }
     connectedCallback() {
         const pageHead = document.getElementsByTagName('head')[0]
-        const script = document.createElement('script')
-        script.src = 'index.js'
-        pageHead.appendChild(script)
         const link = document.createElement('link')
         link.rel = 'stylesheet'
-        link.href = `${window.location.pathname.replace(/\/[^\/]*$/, '/') + 'index.css'}`
+        link.href = `${window.location.origin + '/components/gui/site/index.css'}`
         pageHead.prepend(link)
         document.title = this.title
     }
