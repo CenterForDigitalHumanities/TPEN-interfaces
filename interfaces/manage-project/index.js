797f9dcd84be6454385ab9c44916212743835feb
Patrick Cuba <cubap@slu.edu>
2025-02-24 23:49:34 -0600

    dead code clearout

--- interfaces/manage-project/index.js
+++ interfaces/manage-project/index.js
@@ -3,21 +3,19 @@ import { eventDispatcher } from "../../api/events.mjs"
 import "../../components/project-collaborators/index.js"
 import "../../components/project-details/index.js"
 import "../../components/project-metadata/index.js"
-import "../../components/projects-list/project-list-view.js"
+import "../../components/projects/project-list-view.js"
 
 eventDispatcher.on('tpen-project-loaded', () => render())
 const container = document.body
 TPEN.attachAuthentication(container)
 
 document.getElementById('manage-collaboration-btn').addEventListener('click', () => {
-    const URLParams = new URLSearchParams(window.location.search)
-    const projectID = URLParams.get("projectID")
-    const url = `/interfaces/collaborators.html?projectID=${projectID}`
+    const url = `/interfaces/collaborators.html?projectID=${TPEN.screen.projectInQuery}`
     window.location.href = url
 })
 
 document.getElementById("update-metadata-btn").addEventListener('click', () => {  
-    window.location.href = `/components/update-metadata/index.html?projectID=${TPEN.activeProject._id}`
+    window.location.href = `/components/update-metadata/index.html?projectID=${TPEN.screen.projectInQuery}`
 })
 
 function render() {
