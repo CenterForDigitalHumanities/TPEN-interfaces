8f2594d33edb454fd7c9486b10e524507adf372a
Priyal Patel <mepripri0712@gmail.com>
2025-05-01 16:57:40 -0500

    Update new-action.js

--- components/new-action.js
+++ components/new-action.js
@@ -42,9 +42,8 @@ class NewAction extends HTMLElement {
         })
         this.shadowRoot.getElementById("link-tpen-2.8").addEventListener("click", () => {
             const userToken = localStorage.getItem("userToken")
-            document.cookie = `userToken=${userToken}; path=/; domain=${window.location.origin}; secure; samesite=strict;`
             const redirectUri = encodeURIComponent(`${window.location.origin}/interfaces/import-tpen28/index.html`)
-            window.location.href = `${TPEN.TPEN28URL}/TPEN/login.jsp?redirect_uri=${redirectUri}`
+            window.location.href = `${TPEN.TPEN28URL}/TPEN/login.jsp?redirect_uri=${redirectUri}&userToken=${userToken}`
         })
     }
 }
