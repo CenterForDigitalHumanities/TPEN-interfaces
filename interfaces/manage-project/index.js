ab8e41ae6d28d8cf8843219b005f3cb5020be6b4
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2025-05-27 14:30:36 -0500

    Redirect from manage-project is user is not a owner or leader

--- interfaces/manage-project/index.js
+++ interfaces/manage-project/index.js
@@ -7,6 +7,7 @@ import "../../components/project-permissions/index.js"
 import "../../components/project-export/index.js"
 import "../../components/project-layers/index.js"
 import "../../components/project-tools/index.js"
+import { decodeUserToken } from '../../components/iiif-tools/index.js'
 
 TPEN.eventDispatcher.on('tpen-project-loaded', () => render())
 const container = document.body
@@ -36,17 +37,27 @@ document.getElementById('export-project-btn').addEventListener('click', async ()
 
 function render() {
     document.querySelector('tpen-project-details').setAttribute('tpen-project-id', TPEN.screen.projectInQuery)
-
-    const roles = TPEN.activeProject.roles
-    const permissionList = []
-    Object.values(roles).map((role) => {
-        role.forEach((permission) => {
-            permissionList.push(permission)
-        })
-    })
-    if (Object.keys(roles).includes('OWNER', 'LEADER') || permissionList.includes('*_*_*', '*_*_ROLE', 'CREATE_*_ROLE', 'DELETE_*_ROLE', 'UPDATE_*_ROLE', 'READ_*_ROLE')){
-        document.getElementById("add-custom-role-btn").addEventListener('click', async () => {
-            window.location.href = `/components/manage-role/index.html?projectID=${TPEN.screen.projectInQuery}`  
-        })
+    const collaborators = TPEN.activeProject.collaborators
+    const agent = decodeUserToken(TPEN.getAuthorization())['http://store.rerum.io/agent']
+    const agentRoles = collaborators[agent.split("/").pop()]?.roles
+    const isManager = ["OWNER", "LEADER"].some(role => agentRoles.includes(role))
+    if(!isManager) {
+        alert("You do not have permissions to use this page.")
+        document.location.href = `/project?projectID=${TPEN.screen.projectInQuery}`
     }
+    document.getElementById("add-custom-role-btn").addEventListener('click', async () => {
+        window.location.href = `/components/manage-role/index.html?projectID=${TPEN.screen.projectInQuery}`  
+    })
+    // const roles = TPEN.activeProject.roles
+    // const permissionList = []
+    // Object.values(roles).map((role) => {
+    //     role.forEach((permission) => {
+    //         permissionList.push(permission)
+    //     })
+    // })
+    // if (Object.keys(roles).includes('OWNER', 'LEADER') || permissionList.includes('*_*_*', '*_*_ROLE', 'CREATE_*_ROLE', 'DELETE_*_ROLE', 'UPDATE_*_ROLE', 'READ_*_ROLE')){
+    //     document.getElementById("add-custom-role-btn").addEventListener('click', async () => {
+    //         window.location.href = `/components/manage-role/index.html?projectID=${TPEN.screen.projectInQuery}`  
+    //     })
+    // }
 }
