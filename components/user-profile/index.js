e6e02b31fcec889b9dd5f338d7f3a0da997cd2ed
Priyal Patel <mepripri0712@gmail.com>
2025-06-17 11:32:05 -0500

    User Profile displayName Fix

--- components/user-profile/index.js
+++ components/user-profile/index.js
@@ -170,6 +170,7 @@ class UserProfile extends HTMLElement {
         const saveBtn = this.shadowRoot.querySelector('#saveBtn')
         const cancelBtn = this.shadowRoot.querySelector('#cancelBtn')
         const divBtn = this.shadowRoot.querySelector('.btn-container')
+        const profile = this.shadowRoot.querySelector('#profile')
     
         editBtn.addEventListener('click', () => {
             nameInput.value = nameText.textContent
@@ -227,6 +228,7 @@ class UserProfile extends HTMLElement {
             }
     
             nameText.textContent = newName
+            profile.textContent = `"displayName": "${newName}"`
             nameInput.style.display = 'none'
             nameText.style.display = 'inline'
             emailText.style.display = 'inline'
