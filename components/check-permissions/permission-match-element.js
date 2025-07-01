4324ac1d7194c44259fb35ed2fdd9c80f2ba397a
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2025-07-01 11:24:50 -0500

    changes from review

--- components/check-permissions/permission-match-element.js
+++ components/check-permissions/permission-match-element.js
@@ -1,12 +1,18 @@
 /**
  * A custom element <tpen-can> that acts as a wrapper around components and HTML that require permissions.
  * If the permission supplied does not match on any of the permissions a user has for a project,
- * then alterations occur to the wrapped elements.
+ * then alterations occur to the wrapped elements.  This is processed once when it is loaded
+ * and does not listen for changes.
+ *
+ * Use the tpen-view attribute to check if a user has enough permissions to see the component.
+ * Use the tpen-edit attribute to check if the component should be read-only.  
+ * - If the attribute is not present or is invalid they are considered permitted.
  *
  * To use directly on an HTML interface include this module with a <script> tag in a <head> element like 
  * <script type="module" src="../../components/check-permissions/permission-match-element.js"></script>
- * Use it like
-    <tpen-can tpen-view="ANY_ANY_LINE" tpen-edit="EDIT_*_LINE">
+ *
+ * Ex. Show a tpen-line-component if a user has any Line permissions.  Make it read-only if the user cannot update all Line scopes.
+    <tpen-can tpen-view="ANY_ANY_LINE" tpen-edit="UPDATE_*_LINE">
         <tpen-line-component></tpen-line-component>
     </tpen-can>
  *
@@ -20,6 +26,7 @@ import { getUserFromToken } from "../../components/iiif-tools/index.js"
 import { permissionMatch } from "../../components/check-permissions/permission-match.js"
 
 export class PermissionMatch extends HTMLElement {
+    // TODO use these from a central location, such as a Permission Class.
     #entities = [
         "PROJECT",
         "LAYER",
@@ -32,9 +39,13 @@ export class PermissionMatch extends HTMLElement {
         "*",
         "ANY"
     ]
+    #viewPermission = null
+    #editPermission = null
 
     constructor() {
         super()
+        this.#viewPermission = this.getAttribute("tpen-view")
+        this.#editPermission = this.getAttribute("tpen-edit")
         TPEN.attachAuthentication(this)
         TPEN.eventDispatcher.on("tpen-project-loaded", ev => this.render(ev.detail))
     }
@@ -43,33 +54,27 @@ export class PermissionMatch extends HTMLElement {
 
     render(project) {
         // Must have a loaded project with collaborators or we can't check anything
-        if(!project?.collaborators || !Object.keys(project.collaborators).length) return
+        if (!project?.collaborators || !Object.keys(project.collaborators).length) return
         const userId = getUserFromToken(TPEN.getAuthorization())
         // Must have been on an authenticated interface or we can't check anything
-        if(!userId) return
-        let canView = true
-        let canEdit = true
-        if(this.hasAttribute("tpen-view")) {
-            canView = permissionMatch(this.getAttribute("tpen-view"), project, userId)
-            if(!canView) {
-                this.remove()
-                // No reason to check tpen-edit.  The element is gone.
-                return
-            }
+        if (!userId) return
+        const canView = this.#viewPermission ? permissionMatch(this.#viewPermission, project, userId) : true
+        if (!canView) {
+            this.remove()
+            // No reason to check tpen-edit.  The element is gone.
+            return
         }
-        if(this.hasAttribute("tpen-edit")) {
-            canEdit = permissionMatch(this.getAttribute("tpen-edit"), project, userId)
-            if(!canEdit) {
-                // The element itself
-                this.classList.add("tpen-readonly")
-                this.setAttribute("tpen-readonly", "")
-                // Also mark the element's direct children since those are likely to be components that need to know.
-                if(this.children?.length) {
-                    Array.from(this.children).forEach(child => {
-                        child.classList.add("tpen-readonly")
-                        child.setAttribute("tpen-readonly", "")
-                    })    
-                }
+        const canEdit = this.#editPermission ? permissionMatch(this.#editPermission, project, userId) : true
+        if (!canEdit) {
+            // The element itself
+            this.classList.add("tpen-readonly")
+            this.setAttribute("tpen-readonly", "")
+            // Also mark the element's direct children since those are likely to be components that need to know.
+            if (this.children?.length) {
+                Array.from(this.children).forEach(child => {
+                    child.classList.add("tpen-readonly")
+                    child.setAttribute("tpen-readonly", "")
+                })
             }
         }
     }
--- components/check-permissions/permission-match.js
+++ components/check-permissions/permission-match.js
@@ -1,11 +1,18 @@
 /**
  * A module that processes the "tpen-view" and "tpen-edit" attributes on elements and components throughout the document.
- * If the permission supplied in the attributes does not match on any of the permissions a user has for a project,
- * then alterations occur to the elements. 
+ * If the permission supplied does not match on any of the permissions a user has for a project,
+ * then alterations occur to the wrapped elements.  This is processed once when it is loaded
+ * and does not listen for changes.
+ *
+ * Use the tpen-view attribute to check if the component should be removed as unviewable.
+ * Use the tpen-edit attribute to check if the component should be read-only as uneditable.  
+ * - If the attribute is not present or is invalid they are considered permitted.
  *
  * To use directly on an HTML interface include this module with a <script> tag in a <head> element like 
  * <script type="module" src="../../components/check-permissions/permission-match.js"></script>
- * Including the module automatically checks all elements in the document.  Decorate elements like
+ * Including the module automatically checks all elements in the document.
+ *
+ * Ex. Show a tpen-line-component if a user has any Line permissions.  Make it read-only if the user cannot update all Line scopes.
     <div class="container" tpen-view="ANY_ANY_LINE" tpen-edit="UPDATE_*_LINE">
         <tpen-line-component></tpen-line-component>
     </div>
@@ -19,7 +26,7 @@
 
 import TPEN from '../../api/TPEN.js'
 import { getUserFromToken } from "../../components/iiif-tools/index.js"
-
+// TODO use these from a central location, such as a Permission Class.
 const entities = [
     "PROJECT",
     "LAYER",
@@ -43,17 +50,17 @@ TPEN.eventDispatcher.on("tpen-project-loaded", ev => checkElements(ev.detail))
  */
 function checkElements(project) {
     // Must have a loaded project with collaborators or we can't check anything
-    if(!project?.collaborators || !Object.keys(project.collaborators).length) return
+    if (!project?.collaborators || !Object.keys(project.collaborators).length) return
     const userId = getUserFromToken(TPEN.getAuthorization())
     // Must have been on an authenticated interface or we can't do anything
-    if(!userId) return
+    if (!userId) return
     const elements = document.querySelectorAll("[tpen-view],[tpen-edit]")
     // Why are you using this module if there are no elements to check?
-    if(!elements || elements.length === 0) return
+    if (!elements || elements.length === 0) return
     for (const element of elements) {
         let canView = true
         let canEdit = true
-        if(element.hasAttribute("tpen-view")) {
+        if (element.hasAttribute("tpen-view")) {
             canView = permissionMatch(element.getAttribute("tpen-view"), project, userId)
             // Removes the element (along with everything in it, of course)
             if (!canView) {
@@ -62,21 +69,21 @@ function checkElements(project) {
                 continue
             }
         }
-        if(element.hasAttribute("tpen-edit")) {
+        if (element.hasAttribute("tpen-edit")) {
             canEdit = permissionMatch(element.getAttribute("tpen-edit"), project, userId)
-            if(!canEdit) {
+            if (!canEdit) {
                 // The element itself
                 element.classList.add("tpen-readonly")
                 element.setAttribute("tpen-readonly", "")
                 // Also mark the element's direct children since those are likely to be components that need to know.
-                if(element.children?.length) {
+                if (element.children?.length) {
                     Array.from(element.children).forEach(child => {
                         child.classList.add("tpen-readonly")
                         child.setAttribute("tpen-readonly", "")
-                    })    
+                    })
                 }
-           }
-        } 
+            }
+        }
     }
 }
 
@@ -95,26 +102,26 @@ function checkElements(project) {
 export function permissionMatch(permission, project, userId) {
     // Can't process malformed permission so it is allowed to render.  The value should be a single action_scope_entity string.
     // Check for ',' specifically in case someone tried to supply multiple permissions in the attribute.
-    if(!permission || typeof permission !== "string" || permission.includes(",") || permission.split("_").length !== 3) return true
+    if (!permission || typeof permission !== "string" || permission.includes(",") || permission.split("_").length !== 3) return true
     const provided = permission.split("_").map(ase => ase.toUpperCase())
     // Permissions are project based.  If there is no project then the user is permitted.
-    if(!project) return true
+    if (!project) return true
     // If it isn't an entity we expect then the user is permitted because we have no say in it.
-    if(!provided[2] || !entities.includes(provided[2])) return true
+    if (!provided[2] || !entities.includes(provided[2])) return true
     const userRoles = project?.collaborators?.[userId]?.roles
-    const allUserPermissions = (userRoles && userRoles.length) ? 
+    const allUserPermissions = (userRoles && userRoles.length) ?
         Array.from(new Set(
             userRoles.flatMap(role => project.roles[role])
         )) : []
     // If there are no permissions they will not be permitted.  They might not be in the project.
     let permitted = false
-    for(const permission of allUserPermissions) {
+    for (const permission of allUserPermissions) {
         const current = permission.split("_")
-        permitted = 
-            (provided[0] === "ANY" || current[0] === "*" || provided[0] === current[0])
-         && (provided[1] === "ANY" || current[1] === "*" || provided[1] === current[1])
-         && (provided[2] === "ANY" || current[2] === "*" || provided[2] === current[2])
-         if(permitted) return permitted
+        permitted =
+            (provided[0] === "ANY" || current[0] === "*" || provided[0] === current[0]) &&
+            (provided[1] === "ANY" || current[1] === "*" || provided[1] === current[1]) &&
+            (provided[2] === "ANY" || current[2] === "*" || provided[2] === current[2])
+        if (permitted) return permitted
     }
     return permitted
 }
