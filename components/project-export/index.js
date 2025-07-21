e3eef09aeba89afbf11a80a58bc9a23a30c9fe98
Priyal Patel <mepripri0712@gmail.com>
2025-07-21 12:46:24 -0500

    Project Export Message

--- components/project-export/index.js
+++ components/project-export/index.js
@@ -13,18 +13,25 @@ customElements.define('tpen-project-export', class extends HTMLElement {
     
     async render() {
         const url = `https://dev.static.t-pen.org/${TPEN.activeProject._id}/manifest.json`
+        const response = await fetch(`${TPEN.servicesURL}/project/${TPEN.activeProject._id}/deploymentStatus`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${TPEN.getAuthorization()}`
+            }
+        }).then(response => response.json())
         this.shadowRoot.innerHTML = `
             <style>
-                a {
+                a, .success {
                     display: inline-block;
                     padding: 10px 20px;
-                    color: #007bff;
+                    color: var(--primary-color);
                     text-decoration: none;
                     font-weight: bold;
                     border-radius: 6px;
-                    font-size: 13px;
+                    font-size: 14px;
                     transition: background-color 0.3s ease, transform 0.2s ease;
                     box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.1);
+                    margin: 0;
                 }
 
                 a:hover {
@@ -37,22 +44,30 @@ customElements.define('tpen-project-export', class extends HTMLElement {
                     box-shadow: 1px 1px 6px rgba(0, 0, 0, 0.15);
                 }
 
-                p {
-                    font-size: 16px;
-                    color: red;
+                .error {
+                    display: inline-block;
+                    padding: 10px 20px;
+                    color: #007bff;
+                    text-decoration: none;
                     font-weight: bold;
-                    margin-top: 12px;
+                    border-radius: 6px;
+                    font-size: 14px;
+                    transition: background-color 0.3s ease, transform 0.2s ease;
+                    box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.1);
+                    color: red;
+                    margin: 0;
                 }
             </style>
-
-            ${ await this.checkUrlExists(url) ?
-                `<a href="${url}" target="_blank">
-                    ${url}
-                </a>`
-                :
-                `<p>Manifest Not Found</p>`
-            }
-        `
+            ${  (response.status 
+                ? ((await this.checkUrlExists(url) && response.message !== 'Manifest found, Recently Committed') 
+                    ? `<a href="${url}" target="_blank">
+                        ${url}
+                        </a>` 
+                    : ((response.message === 'Manifest found, Recently Committed') 
+                        ? `<p class="success">Successfully Exporting Project Manifest... Please Wait</p>` 
+                        : `<p class="error">Manifest Not Found</p>`))
+                : `<p class="success">Successfully Exporting Project Manifest... Please Wait</p>`)
+            }`
     }
 
     async checkUrlExists(url) {
