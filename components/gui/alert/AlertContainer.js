330cbfbf52bfbc0317a9fac2647c9e6263e4b7f5
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2025-08-05 14:35:05 -0500

    changes from testing

--- components/gui/alert/AlertContainer.js
+++ components/gui/alert/AlertContainer.js
@@ -42,7 +42,17 @@ class AlertContainer extends HTMLElement {
 
     render() {
         const style = document.createElement('style')
+        // FIXME we copied the :root rules from /components/gui/site/index.css.  Importing it was too much.
         style.textContent = `
+            :host {
+              --primary-color: hsl(186, 84%, 40%);
+              --primary-light: hsl(186, 84%, 60%);
+              --light-color  : hsl(186, 84%, 90%);
+              --dark         : #2d2d2d;
+              --white        : hsl(0, 0%, 100%);
+              --gray         : hsl(0, 0%, 60%);
+              --light-gray   : hsl(0, 0%, 90%);
+            }
             .alert-area {
                 position: fixed;
                 display: grid;
@@ -52,7 +62,7 @@ class AlertContainer extends HTMLElement {
                 justify-items: center;
                 justify-content: center;
                 height: 0vh;
-                background-color: rgba(0,0,0,0.5);
+                background-color: rgba(0,0,0,0.7);
                 opacity: 0;
                 transition: all 0.5s ease-in-out;   
             }
@@ -71,11 +81,14 @@ class AlertContainer extends HTMLElement {
                 box-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
                 opacity: 0.0;
                 height: fit-content;
-                transition: all 0.3s ease-in-out;   
+                min-width: 25vw;
+                max-width: 35vw;
+                transition: all 0.3s ease-in-out;
+                font-size: 14pt;
             }
             .alert-area tpen-alert {
                 top: 0px;
-                right: 20px;
+                right: 0px;
             }
             @media (prefers-reduced-motion) {
                 .alert-area tpen-alert { 
@@ -83,6 +96,12 @@ class AlertContainer extends HTMLElement {
                     height: fit-content;
                     top: 5vh;
                 }
+            }
+            .alert-area .button-container {
+                position: relative;
+                display: block;
+                text-align: right;
+                margin-top: 1vh;
             }
             .alert-area tpen-alert.show {
                 opacity: 1.0;
@@ -92,11 +111,21 @@ class AlertContainer extends HTMLElement {
             .alert-area button {
                 position: relative;
                 display: inline-block;
+                cursor: pointer;
+                border: none;
+                padding: 10px 20px;
+                background-color: var(--primary-color);
+                outline: var(--primary-light) 1px solid;
+                outline-offset: -3.5px;
+                color: var(--white);
+                border-radius: 5px;
+                transition: all 0.3s;
+                font-size: 12pt;
             }
-            .alert-area .button-container {
-                position: relative;
-                display: block;
-                text-align: right;
+            .alert-area button:hover {
+                background-color: var(--primary-light);
+                outline: var(--primary-color) 1px solid;
+                outline-offset: -1.5px;
             }
         `
         // This section will take over the screen and lock down screen interaction.  It lives at the top of the viewport.
--- components/gui/confirm/ConfirmContainer.js
+++ components/gui/confirm/ConfirmContainer.js
@@ -33,6 +33,7 @@ class ConfirmContainer extends HTMLElement {
         buttonContainer.classList.add("button-container")
         const confirmElem = document.createElement('tpen-confirm')
         const confirmButton = document.createElement('button')
+        confirmButton.style.marginRight = "10px"
         const denyButton = document.createElement('button')
         confirmElem.textContent = message
         confirmButton.textContent = positiveButtonText
@@ -55,7 +56,17 @@ class ConfirmContainer extends HTMLElement {
 
     render() {
         const style = document.createElement('style')
+        // FIXME we copied the :root rules from /components/gui/site/index.css.  Importing it was too much.
         style.textContent = `
+            :host {
+              --primary-color: hsl(186, 84%, 40%);
+              --primary-light: hsl(186, 84%, 60%);
+              --light-color  : hsl(186, 84%, 90%);
+              --dark         : #2d2d2d;
+              --white        : hsl(0, 0%, 100%);
+              --gray         : hsl(0, 0%, 60%);
+              --light-gray   : hsl(0, 0%, 90%);
+            }
             .confirm-area {
                 position: fixed;
                 display: grid;
@@ -65,7 +76,7 @@ class ConfirmContainer extends HTMLElement {
                 justify-items: center;
                 justify-content: center;
                 height: 0vh;
-                background-color: rgba(0,0,0,0.5);
+                background-color: rgba(0,0,0,0.7);
                 opacity: 0;
                 transition: all 0.5s ease-in-out;   
             }
@@ -84,11 +95,14 @@ class ConfirmContainer extends HTMLElement {
                 box-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
                 opacity: 0.0;
                 height: fit-content;
-                transition: all 0.3s ease-in-out;   
+                min-width: 25vw;
+                max-width: 35vw;
+                transition: all 0.3s ease-in-out;
+                font-size: 14pt;
             }
             .confirm-area tpen-confirm {
                 top: 0px;
-                right: 20px;
+                right: 0px;
             }
             @media (prefers-reduced-motion) {
                 .confirm-area tpen-confirm { 
@@ -102,14 +116,30 @@ class ConfirmContainer extends HTMLElement {
                 height: fit-content;
                 top: 5vh;
             }
-            .confirm-area button {
-                position: relative;
-                display: inline-block;
-            }
             .confirm-area .button-container {
                 position: relative;
                 display: block;
                 text-align: right;
+                margin-top: 1vh;
+            }
+            .confirm-area button {
+                position: relative;
+                display: inline-block;
+                cursor: pointer;
+                border: none;
+                padding: 10px 20px;
+                background-color: var(--primary-color);
+                outline: var(--primary-light) 1px solid;
+                outline-offset: -3.5px;
+                color: var(--white);
+                border-radius: 5px;
+                transition: all 0.3s;
+                font-size: 12pt;
+            }
+            .confirm-area button:hover {
+                background-color: var(--primary-light);
+                outline: var(--primary-color) 1px solid;
+                outline-offset: -1.5px;
             }
         `
         // This section will take over the screen and lock down screen interaction.  It lives at the top of the viewport.
