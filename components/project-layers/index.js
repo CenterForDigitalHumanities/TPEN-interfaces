63ca50597e0b53a838bb7e1ea44ec36c1ac0824b
mepripri <52342511+mepripri@users.noreply.github.com>
2025-08-06 13:00:45 -0500

    CSS

--- components/project-layers/index.js
+++ components/project-layers/index.js
@@ -15,85 +15,85 @@ class ProjectLayers extends HTMLElement {
         const layers = TPEN.activeProject.layers
         this.shadowRoot.innerHTML = `
             <style>
-            .container {
-                display: flex;
-                flex-direction: column;
-                gap: 10px;
-            }
-
-            .layer-card {
-                background: #fff;
-                border-radius: 8px;
-                box-shadow: 0 1px 2px rgba(0, 0, 0, 0.4);
-                user-select: text;
-                overflow: hidden;
-                display: flex;
-                flex-direction: column;
-            }
-
-            .info-list {
-                display: flex;
-                flex-direction: column;
-                list-style: none;
-                padding: 0;
-                margin: 0;
-                flex: 1;
-            }
-
-            .info-item {
-                display: flex;
-                padding: 8px 20px;
-                border-radius: 4px;
-            }
-
-            .info-item span {
-                display: inline-block;
-                overflow-wrap: break-word;
-                word-break: break-word;
-                line-height: 1.5;
-            }
-
-            .info-label {
-                font-weight: bold;
-                width: 20%;
-                font-size: 0.9em;
-                vertical-align: top;
-                color: var(--primary-color, #007bff);
-                user-select: text;
-            }
-
-            .info-value {
-                width: 80%;
-                font-size: 0.9em;
-                margin-left: auto;
-                user-select: text;
-            }
-
-            .footer {
-                display: flex;
-                justify-content: flex-end;
-                align-items: center;
-                padding: 4px 16px 4px 8px;
-                border-bottom-left-radius: 8px;
-                border-bottom-right-radius: 8px;
-                font-size: 13px;
-                font-weight: 500;
-                background-color: var(--light-color);
-                box-shadow: 0 1px 2px rgba(0, 0, 0, 0.4);
-            }
-
-            .footer-label {
-                border-radius: 4px;
-                color: var(--primary-color);
-                user-select: text;
-                letter-spacing: 0.5px;
-            }
-
-            @media (max-width: 1080px) {
-                .info-list {
-                height: 18em;
+                .container {
+                    display: flex;
+                    flex-direction: column;
+                    gap: 10px;
                 }
-            }
+
+                .layer-card {
+                    background: #fff;
+                    border-radius: 8px;
+                    box-shadow: 0 1px 2px rgba(0, 0, 0, 0.4);
+                    user-select: text;
+                    overflow: hidden;
+                    display: flex;
+                    flex-direction: column;
+                }
+
+                .info-list {
+                    display: flex;
+                    flex-direction: column;
+                    list-style: none;
+                    padding: 0;
+                    margin: 0;
+                    flex: 1;
+                }
+
+                .info-item {
+                    display: flex;
+                    padding: 8px 20px;
+                    border-radius: 4px;
+                }
+
+                .info-item span {
+                    display: inline-block;
+                    overflow-wrap: break-word;
+                    word-break: break-word;
+                    line-height: 1.5;
+                }
+
+                .info-label {
+                    font-weight: bold;
+                    width: 20%;
+                    font-size: 0.9em;
+                    vertical-align: top;
+                    color: var(--primary-color, #007bff);
+                    user-select: text;
+                }
+
+                .info-value {
+                    width: 80%;
+                    font-size: 0.9em;
+                    margin-left: auto;
+                    user-select: text;
+                }
+
+                .footer {
+                    display: flex;
+                    justify-content: flex-end;
+                    align-items: center;
+                    padding: 4px 16px 4px 8px;
+                    border-bottom-left-radius: 8px;
+                    border-bottom-right-radius: 8px;
+                    font-size: 13px;
+                    font-weight: 500;
+                    background-color: var(--light-color);
+                    box-shadow: 0 1px 2px rgba(0, 0, 0, 0.4);
+                }
+
+                .footer-label {
+                    border-radius: 4px;
+                    color: var(--primary-color);
+                    user-select: text;
+                    letter-spacing: 0.5px;
+                }
+
+                @media (max-width: 1080px) {
+                    .info-list {
+                    height: 18em;
+                    }
+                }
             </style>
 
             <div class="container">
