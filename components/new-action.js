4c0278f211e594257bb9fea30c5a22050d95c32f
mepripri <52342511+mepripri@users.noreply.github.com>
2025-09-08 13:02:57 -0500

    TPEN28 Cookies Change

--- components/new-action.js
+++ components/new-action.js
@@ -64,20 +64,34 @@ class NewAction extends HTMLElement {
         this.shadowRoot.getElementById("link-tpen-2.8").addEventListener("click", this.TPEN2ImportHandler.bind(this))
     }
 
-    TPEN2ImportHandler = (event) => {
-        event.preventDefault()
+    TPEN2ImportHandler = async (event) => {
+        event.preventDefault();
         const userToken = localStorage.getItem("userToken")
         let tokenDomain
-
+        let isProd
+    
         if (TPEN.TPEN28URL.includes("t-pen.org")) {
             tokenDomain = "t-pen.org"
+            isProd = true
         }
 
         if (TPEN.TPEN28URL.includes("localhost")) {
             tokenDomain = "localhost"
         }
-        
-        document.cookie = `userToken=${userToken}; path=/; domain=${tokenDomain}; secure; samesite=strict;`;    
+    
+        let cookieString = `userToken=${userToken}; domain=${tokenDomain}; path=/; SameSite=Strict;`
+    
+        if (isProd) {
+            cookieString += " Secure;"
+        }
+    
+        document.cookie = cookieString
+        await fetch(`${TPEN.servicesURL}/project/deletecookie`, {
+            method: "GET",
+            credentials: "include",
+        })
+
+        document.cookie = cookieString
         const redirectUri = encodeURIComponent(`${window.location.origin}/project/import28`)
         window.location.href = `${TPEN.TPEN28URL}/TPEN/login.jsp?redirect_uri=${redirectUri}`
     }
