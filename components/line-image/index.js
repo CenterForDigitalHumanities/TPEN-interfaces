35b2c739de93a67568d703fd60d079d81cdc5bf5
mepripri <52342511+mepripri@users.noreply.github.com>
2025-10-24 09:35:47 -0500

    transition

--- components/line-image/index.js
+++ components/line-image/index.js
@@ -93,13 +93,11 @@ class TpenLineImage extends HTMLElement {
         canvasPanel.setAttribute("canvas-id", this.#canvas)
         canvasPanel.setAttribute('region', `${x},${y},${width},${height}`)
 
-        canvasPanel.style.transform = 'translateY(100%)'
-        canvasPanel.style.transition = `transform ${duration}ms cubic-bezier(0.19, 1, 0.22, 1)`
-        canvasPanel.style.width = '100%'
-        canvasPanel.style.height = '100%'
+        canvasPanel.style.opacity = '0'
+        canvasPanel.style.transition = `opacity ${duration}ms ease-in-out`
 
         requestAnimationFrame(() => {
-            canvasPanel.style.transform = 'translateY(0)'
+            canvasPanel.style.opacity = '1'
         })
 
         this.shadowRoot.append(canvasPanel)
