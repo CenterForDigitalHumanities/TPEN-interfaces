8f7a4d98e143613c240d9b30847ff15f7492eda1
Patrick Cuba <cubap@slu.edu>
2025-10-28 15:44:45 -0500

    reorder

--- api/config.js
+++ api/config.js
@@ -6,20 +6,20 @@ export const ACTIVE_ENV = 'dev' // 'dev' | 'prod'
 
 export const ENVIRONMENTS = {
   dev: {
+    BASEURL: 'https://dev.app.t-pen.org',
     servicesURL: 'https://dev.api.t-pen.org',
     tinyThingsURL: 'https://dev.tiny.t-pen.org',
     RERUMURL: 'https://devstore.rerum.io/v1',
     TPEN28URL: 'https://t-pen.org',
     TPEN3URL: 'https://three.t-pen.org',
-    BASEURL: 'https://app.t-pen.org'
   },
   prod: {
+    BASEURL: 'https://app.t-pen.org',
     servicesURL: 'https://api.t-pen.org',
     tinyThingsURL: 'https://tiny.t-pen.org',
     RERUMURL: 'https://store.rerum.io/v1',
     TPEN28URL: 'https://t-pen.org',
     TPEN3URL: 'https://three.t-pen.org',
-    BASEURL: 'https://app.t-pen.org'
   }
 }
 
