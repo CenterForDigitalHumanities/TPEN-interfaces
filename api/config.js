9e1242dcb8035642f44f20a0102659bf11926b4d
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2026-02-04 13:39:32 -0600

    changes while reviewing

--- api/config.js
+++ api/config.js
@@ -22,7 +22,7 @@ export const ACTIVE_ENV = (
 
 export const ENVIRONMENTS = {
   dev: {
-    BASEURL: 'http://localhost:4000',
+    BASEURL: 'https://dev.app.t-pen.org',
     servicesURL: 'https://dev.api.t-pen.org',
     tinyThingsURL: 'https://dev.tiny.t-pen.org',
     staticURL: 'https://dev.static.t-pen.org',
