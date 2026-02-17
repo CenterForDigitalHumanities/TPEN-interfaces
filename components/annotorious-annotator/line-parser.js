459a845bcf5991e1d543b29d743f28f852bd1ab1
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2026-02-17 14:12:18 -0600

    changes while reviewing

--- components/annotorious-annotator/line-parser.js
+++ components/annotorious-annotator/line-parser.js
@@ -1084,7 +1084,9 @@ class AnnotoriousAnnotator extends HTMLElement {
     let page = JSON.parse(JSON.stringify(this.#resolvedAnnotationPage))
     page.items = allAnnotations
     const pageID = page["@id"] ?? page.id
-    const mod = await fetch(`${TPEN.servicesURL}/project/${TPEN.activeProject._id}/page/${pageID.split("/").pop()}`, {
+    let mod
+    try {
+      const res = await fetch(`${TPEN.servicesURL}/project/${TPEN.activeProject._id}/page/${pageID.split("/").pop()}`, {
         method: "PUT",
         headers: {
           "Content-Type": "application/json",
@@ -1092,20 +1094,18 @@ class AnnotoriousAnnotator extends HTMLElement {
         },
         body: JSON.stringify({ "items": page.items })
       })
-      .then(res => {
-        if(!res.ok) {
-          TPEN.eventDispatcher.dispatch("tpen-toast", {
-            message: "ERROR Annotations Not Saved",
-            status: "error"
-          })
-          throw new Error("Could not save annotations", { "cause": `\n${res.status} Error from TPEN Services.  Check the Network response.` })
-        }
-        return res.json()
-      })
-      .catch(err => {
-        saveButton.textContent = "ERROR"
-        throw err
-      })
+      if (!res.ok) {
+        TPEN.eventDispatcher.dispatch("tpen-toast", {
+          message: "ERROR Annotations Not Saved",
+          status: "error"
+        })
+        throw new Error("Could not save annotations", { "cause": `\n${res.status} Error from TPEN Services.  Check the Network response.` })
+      }
+      mod = await res.json()
+    } catch (err) {
+      saveButton.textContent = "ERROR"
+      throw err
+    }
     page.items = page.items.map(i => {
       const selectorValue = i.target?.selector?.value ?? i.target
       // Prefer matching by ID for previously-saved annotations, fall back to selector for new ones
@@ -1119,7 +1119,7 @@ class AnnotoriousAnnotator extends HTMLElement {
     this.#modifiedAnnotationPage = page
     this.#resolvedAnnotationPage = JSON.parse(JSON.stringify(page))
     // Sync server-assigned IDs back to Annotorious so subsequent saves use the correct IDs
-    let syncAnnotations = page.items
+    let syncAnnotations = JSON.parse(JSON.stringify(page.items))
     syncAnnotations = this.formatAnnotations(syncAnnotations)
     syncAnnotations = this.convertSelectors(syncAnnotations, true)
     this.#annotoriousInstance.clearAnnotations()
